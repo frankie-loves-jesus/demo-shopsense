bf696703611fa5f311fad88bab6ec4ed96e43df2
Frankie <orangeprince@hushmail.com>
2014-04-18 17:35:40 +0200

    rails new demo-shopsense

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoShopsense::Application.config.secret_key_base = 'a40b4f85a509db6af51497794f0b799120ac861d142859c3414bc092739165c3a76eeec1dd0f2c1ad9563cf0ff953bb1e1d07bd19ea29edcf6276e6859f6693b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemoShopsense::Application.config.session_store :cookie_store, key: '_demo-shopsense_session'
